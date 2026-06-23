c15ef7d6e6ba6bf63d25c6152eeb4d1190e51528
Ilia <ikoldercov@gmail.com>
2026-06-23 17:11:58 +0900

    Fix service worker cache paths

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,19 @@
-const CACHE_NAME = 'shinobi-jlpt-academy-pwa-60353c678cfe';
+const CACHE_NAME = 'shinobi-jlpt-academy-pwa-root-fix-v1';
 const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./shinobi_images/images%20(1).jfif",
-  "./shinobi_images/images%20(2).jfif",
-  "./shinobi_images/images%20(3).jfif",
-  "./shinobi_images/images%20(4).jfif",
-  "./shinobi_images/images%20(5).jfif",
-  "./shinobi_images/images%20(6).jfif",
-  "./shinobi_images/images%20(7).jfif",
-  "./shinobi_images/images%20(8).jfif",
-  "./shinobi_images/images.jfif",
-  "./icons/apple-touch-icon.png",
-  "./icons/icon-128.png",
-  "./icons/icon-144.png",
-  "./icons/icon-152.png",
-  "./icons/icon-180.png",
-  "./icons/icon-192.png",
-  "./icons/icon-384.png",
-  "./icons/icon-512.png",
-  "./icons/icon-72.png",
-  "./icons/icon-96.png"
+  './',
+  './index.html',
+  './shinobi_jlpt_academy.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './images.jfif',
+  './images (1).jfif',
+  './images (3).jfif',
+  './images (4).jfif',
+  './images (5).jfif',
+  './images (6).jfif',
+  './images (7).jfif',
+  './images (8).jfif'
 ];
 
 self.addEventListener('install', event => {
@@ -36,7 +28,7 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME && key.startsWith('shinobi-jlpt-academy-pwa-')).map(key => caches.delete(key))
+      keys.filter(key => key.startsWith('shinobi-jlpt-academy-pwa-') && key !== CACHE_NAME).map(key => caches.delete(key))
     )).then(() => self.clients.claim())
   );
 });
@@ -49,9 +41,9 @@ self.addEventListener('fetch', event => {
     event.respondWith(
       fetch(req).then(res => {
         const copy = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+        caches.open(CACHE_NAME).then(cache => cache.put('./shinobi_jlpt_academy.html', copy));
         return res;
-      }).catch(() => caches.match('./index.html'))
+      }).catch(() => caches.match('./shinobi_jlpt_academy.html') || caches.match('./index.html'))
     );
     return;
   }
